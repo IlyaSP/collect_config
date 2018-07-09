e492afd9bd60c4fb90744d7c1d306077c36af13b
Ilya <37418897+IlyaSP@users.noreply.github.com>
2018-07-09 16:06:34 +0300

    Add juniper statistic

--- work_with_sql.py
+++ work_with_sql.py
@@ -76,6 +76,32 @@ def get_snmp_Cisco(community, ip):
     return (s)
 
 
+def get_snmp_juniper(community, ip):
+    """get information snmp for Juniper"""
+    # print("Get snmp for Juniper\n")
+    cmdGen = cmdgen.CommandGenerator()
+    # print('community, ip = ' + str(community) + ' ' + str(ip))
+    errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+        cmdgen.CommunityData(community),
+        cmdgen.UdpTransportTarget((ip, 161)), '.1.3.6.1.4.1.2636.3.1.13.1.8.9.1.0.0',
+        '.1.3.6.1.4.1.2636.3.1.13.1.8.9.1.0.0')
+    if varBinds == []:
+        s = [0, 0]
+    else:
+        s = []
+        for i in varBinds:
+            # print(i)
+            a = str(i)
+            if a.find('No Such ') == -1:
+                s.append(int(a[a.find('=') + 2:]))
+            else:
+                s.append(0)
+        # print('s = ' + str(s))
+
+    # print('s = ' + str(s))
+    return (s)
+
+
 def insert_data_databese(load_1m, load_5m):
     date_added = datetime.datetime.now()
     # print('date_added', date_added)
@@ -100,7 +126,7 @@ def update_data_in_database(id_string, load_1m, load_5m):
 cur.execute('SELECT * FROM collect_configs_device WHERE status = "OK"')
 res = cur.fetchall()
 # print(res)
-
+print("Number of devices: {0}".format(len(res)))
 for i in res:
     # print(i[0], i[1], i[8], i[10])
     community = i[8]
@@ -108,7 +134,7 @@ for i in res:
     device_id = int(i[0])
     vendor_or_OS = i[10]
     old_status = i[11]
-    print(' -- = {0} =---= {1} =--- Status = {2}= --'.format(str(vendor_or_OS), str(ip), str(old_status)))
+    # print(' -- = {0} =---= {1} =--- Status = {2}= --'.format(str(vendor_or_OS), str(ip), str(old_status)))
     if vendor_or_OS == 'Linux':
         # print('- device_id = {0}; ip = {1}; Vendor ={2} ---'.format(str(device_id), str(ip), str(vendor_or_OS)))
         cur.execute('SELECT * FROM collect_configs_statisticsdevice WHERE device_id = ? ORDER BY date_added DESC',
@@ -170,6 +196,38 @@ for i in res:
             else:
                 # print('---------------------------- The time has not yet expired -----------------------')
                 load_1m, load_5m = get_snmp_Cisco(community, ip)
+                insert_data_databese(load_1m, load_5m)
+                # print('----------------- END (The time has not yet expired)-----------------------------')
+                print("====== Data for {0} was added =======".format(ip))
+    elif vendor_or_OS == 'Juniper':
+        # print('- device_id = {0}; ip = {1}; Vendor ={2} ---'.format(str(device_id), str(ip), str(vendor_or_OS)))
+        cur.execute('SELECT * FROM collect_configs_statisticsdevice WHERE device_id = ? ORDER BY date_added DESC',
+                    (str(device_id),))
+        l = cur.fetchall()
+        # print("+++++++ sum string for device = {0} +++++++++".format(len(l)))
+        if len(l) < 2:
+            # print('-----------------l < 2----------------------------------')
+            load_1m, load_5m = get_snmp_juniper(community, ip)
+            # print('load_1m = ' + str(load_1m), load_5m)
+            insert_data_databese(load_1m, load_5m)
+            # print('------------------- END l < 2 ---------------------------------------')
+            print("====== Data for {0} was added =======".format(ip))
+        else:
+            # print('------------------- l > 2 -------------------------------------------')
+            delta = datetime.datetime.strptime(l[0][4], "%Y-%m-%d %H:%M:%S.%f") - datetime.datetime.strptime(
+                l[len(l) - 1][4], "%Y-%m-%d %H:%M:%S.%f")
+            # print("---------------------- Delta in seconds = {0}--------------------".format(delta.total_seconds()))
+            if delta.total_seconds() >= 7200:
+                # print('-------------- Time is up -------------------')
+                id_string = int(l[len(l) - 1][0])
+                # print('------ Id string for update: {0} ---------------'.format(str(id_string)))
+                load_1m, load_5m = get_snmp_juniper(community, ip)
+                update_data_in_database(id_string, load_1m, load_5m)
+                # print('--------------------- END l > 2(Time is up) -------------------------------------')
+                print("====== Data for {0} was updated =======".format(ip))
+            else:
+                # print('---------------------------- The time has not yet expired -----------------------')
+                load_1m, load_5m = get_snmp_juniper(community, ip)
                 insert_data_databese(load_1m, load_5m)
                 # print('----------------- END (The time has not yet expired)-----------------------------')
                 print("====== Data for {0} was added =======".format(ip))
